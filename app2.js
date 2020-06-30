bb6709cb49dad1769e48ca7fcef7f9c9c3b3af45
Matthias Harksen <matthias.harksen@gmail.com>
2020-06-30 11:24:21 +0000

    Bætti við EuPhO dæmum

--- app2.js
+++ app2.js
@@ -1,24 +1,23 @@
-const navSlide = () => {
+const subNavSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
+	const subNavLinks = document.querySelectorAll('nav ul li ul li')
 
 
-	burger.addEventListener('click',()=> {
+	navLinks.addEventListener('click',()=> {
 		// Toggle Nav
-		nav.classList.toggle('nav-active');
+		navLinks.classList.toggle('navLinks-active');
 
 		// Animate Links
-		navLinks.forEach((link, index) => {
+		subNavLinks.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = '';
 			} else {
 				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 30 + 0.1}s`;
 			}
 		});
-		//Burger Animation
-		burger.classList.toggle('toggle');
 	});
 }
 
-navSlide();
+subNavSlide();
